0b9fa63806106c45f506dd34bf7a029fba03d2cb
JuanPGomezB <jugomezb@unal.edu.co>
2025-02-28 18:27:34 -0500

    Modificación ButtonTransform

--- Frontend/gestor Omnitrix/src/UI/ButtonTransform.jsx
+++ Frontend/gestor Omnitrix/src/UI/ButtonTransform.jsx
@@ -1,30 +1,3 @@
-// import { useAliens } from "../contexts/AliensContext";
-// import styles from "./ButtonTransform.module.css";
-//
-// function ButtonTransform({ selectedAlien }) {
-//   const { transformAlien } = useAliens();
-//   const handleTransform = () => {
-//     if (selectedAlien) {
-//       transformAlien(selectedAlien);
-//     }
-//   };
-//
-//   return (
-//     <button
-//       // className="rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
-//       className={`${styles.neon__button} `}
-//       onClick={handleTransform}
-//     >
-//       <div className={styles.reloj} />
-//       <div className={styles.rombo}>
-//         <img src={`/${selectedAlien.name}.png`} />
-//       </div>
-//     </button>
-//   );
-// }
-//
-// export default ButtonTransform;
-
 import {useAliens} from "../contexts/AliensContext";
 import styles from "./ButtonTransform.module.css";
 
