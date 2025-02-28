2b336642a47267a9c06f8b2e49ba7306d3d34aff
JuanPGomezB <jugomezb@unal.edu.co>
2025-02-28 18:15:51 -0500

    Modificación ButtonTransform

--- Frontend/gestor Omnitrix/src/UI/ButtonTransform.jsx
+++ Frontend/gestor Omnitrix/src/UI/ButtonTransform.jsx
@@ -1,8 +1,35 @@
-import { useAliens } from "../contexts/AliensContext";
+// import { useAliens } from "../contexts/AliensContext";
+// import styles from "./ButtonTransform.module.css";
+//
+// function ButtonTransform({ selectedAlien }) {
+//   const { transformAlien } = useAliens();
+//   const handleTransform = () => {
+//     if (selectedAlien) {
+//       transformAlien(selectedAlien);
+//     }
+//   };
+//
+//   return (
+//     <button
+//       // className="rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
+//       className={`${styles.neon__button} `}
+//       onClick={handleTransform}
+//     >
+//       <div className={styles.reloj} />
+//       <div className={styles.rombo}>
+//         <img src={`/${selectedAlien.name}.png`} />
+//       </div>
+//     </button>
+//   );
+// }
+//
+// export default ButtonTransform;
+
+import {useAliens} from "../contexts/AliensContext";
 import styles from "./ButtonTransform.module.css";
 
-function ButtonTransform({ selectedAlien }) {
-  const { transformAlien } = useAliens();
+function ButtonTransform({selectedAlien}) {
+  const {transformAlien} = useAliens();
   const handleTransform = () => {
     if (selectedAlien) {
       transformAlien(selectedAlien);
@@ -10,17 +37,26 @@ function ButtonTransform({ selectedAlien }) {
   };
 
   return (
-    <button
-      // className="rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
-      className={`${styles.neon__button} `}
-      onClick={handleTransform}
-    >
-      <div className={styles.reloj} />
-      <div className={styles.rombo}>
-        <img src={`/${selectedAlien.name}.png`} />
-      </div>
-    </button>
+      <button
+          className={styles.neon__button}
+          onClick={handleTransform}
+          style={{
+            backgroundImage: `url(/OmnitrixInicial.png)`,
+            backgroundSize: "150%",
+            backgroundPosition: "center",
+          }}
+      >
+        <div className={styles.reloj}/>
+        <div className={styles.rombo}>
+          <img
+              className={styles.alienSilhouette}
+              src={`/${selectedAlien.name}.png`}
+              alt={selectedAlien.name}
+          />
+        </div>
+      </button>
   );
 }
 
 export default ButtonTransform;
+
